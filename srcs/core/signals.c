9b77f5dec7104ce47eda2d629ebfea259a47c622
Djihane Bouizem <djihane.bouizem@gmail.com>
2026-01-23 06:04:30 +0100

    fix: prompt signal handling and Ctrl C display

--- srcs/core/signals.c
+++ srcs/core/signals.c
@@ -12,63 +12,37 @@
 
 #include "../../includes/minishell.h"
 
-#ifndef ECHOCTL
-# define ECHOCTL 0
-#endif
-
-static int	is_echoctl_enabled(void)
-{
-	struct termios	term;
-
-	if (tcgetattr(STDIN_FILENO, &term) == -1)
-		return (0);
-	return ((term.c_lflag & ECHO) && (term.c_lflag & ECHOCTL));
-}
-
 static int	readline_getc(FILE *stream)
 {
 	unsigned char	c;
 	ssize_t			nread;
 
+	(void)stream;
 	while (1)
 	{
-		nread = read(fileno(stream), &c, 1);
+		nread = read(STDIN_FILENO, &c, 1);
 		if (nread == 1)
 			return (c);
 		if (nread == 0)
 			return (EOF);
-		if (errno == EINTR)
+		if (errno != EINTR)
+			return (EOF);
+		if (g_signal == SIGQUIT
+			&& (!rl_line_buffer || !rl_line_buffer[0]))
 		{
-			if (g_signal == SIGINT || g_signal == SIGQUIT)
-				return ('\n');
+			g_signal = 0;
 			continue ;
 		}
-		return (EOF);
+		if (g_signal == SIGINT || g_signal == SIGQUIT)
+			return ('\n');
 	}
 }
 
 static void	handle_prompt_signal(int signo)
 {
-	if (!isatty(STDIN_FILENO) || !isatty(STDOUT_FILENO))
-		return ;
-	if (signo == SIGQUIT && (!rl_line_buffer || !rl_line_buffer[0]))
-		return ;
 	g_signal = signo;
 	if (signo == SIGINT)
-	{
-		rl_done = 1;
-		if (!is_echoctl_enabled())
-			write(STDOUT_FILENO, "^C\n", 3);
-		else
-			write(STDOUT_FILENO, "\n", 1);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-		return ;
-	}
-	rl_done = 1;
-	rl_replace_line("", 0);
-	write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, "^C", 2);
 }
 
 void	setup_child_signals(void)
