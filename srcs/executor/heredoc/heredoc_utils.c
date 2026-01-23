d1a24a184494a8af519a3c5c73323882e851cd78
Djihane Bouizem <djihane.bouizem@gmail.com>
2026-01-23 06:32:32 +0100

    fix: Remove heredoc echoctl global and simplify SIGINT handler

--- srcs/executor/heredoc/heredoc_utils.c
+++ srcs/executor/heredoc/heredoc_utils.c
@@ -1,24 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbouizem <djihane.bouizem@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 18:40:00 by dbouizem          #+#    #+#             */
+/*   Updated: 2025/12/08 18:40:00 by dbouizem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../includes/minishell.h"
-
-#ifdef ECHOCTL
-
-static int	read_echoctl_flag(void)
-{
-	struct termios	term;
-
-	if (tcgetattr(STDIN_FILENO, &term) == 0)
-		return ((term.c_lflag & ECHO) && (term.c_lflag & ECHOCTL));
-	return (0);
-}
-#else
-
-static int	read_echoctl_flag(void)
-{
-	return (0);
-}
-#endif
-
-static int	g_heredoc_echoctl;
 
 void	restore_signals(struct sigaction *old_int,
 		struct sigaction *old_quit)
@@ -31,13 +23,7 @@ void	heredoc_sigint_handler(int signo)
 {
 	(void)signo;
 	g_signal = SIGINT;
-	if (isatty(STDIN_FILENO))
-	{
-		if (!g_heredoc_echoctl)
-			write(STDOUT_FILENO, "^C\n", 3);
-		else
-			write(STDOUT_FILENO, "\n", 1);
-	}
+	write(STDOUT_FILENO, "\n", 1);
 }
 
 void	setup_heredoc_signals(struct sigaction *old_int,
@@ -45,7 +31,6 @@ void	setup_heredoc_signals(struct sigaction *old_int,
 {
 	struct sigaction	sa;
 
-	g_heredoc_echoctl = read_echoctl_flag();
 	sa.sa_handler = heredoc_sigint_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
